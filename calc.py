5962efbd9d7554e6169a02e0511ec4234c133999
Baconator <34716421+TSHSTORE@users.noreply.github.com>
2021-11-24 15:43:09 -0600

    Added the Calculator
    This is the calculator. The script (only script btw) is here

---
+++ calc.py
@@ -0,0 +1,40 @@
+from typing import AnyStr
+
+
+oggn: AnyStr
+print("Welcome To The [BaconSoft] Calculator!")
+print("What is your name?")
+name = input("My name is: ")
+space = (" ")
+ioq = ("Thanks, ")
+ioqq = ("!\n")
+ioqqq = ("Alright, ")
+ioo = ("Ok, ")
+io = ("Enter The First Number Of Your Math Problem!")
+iuy = ("Enter The Second Number Of Your Math Problem!")
+ioj = ("Enter your operation!\n [1] for addition\n [2] for subtraction\n [3] for mutiliplacation\n [4] for division\n")
+print(ioo + name + space + io)
+i = int(input())
+print(ioq + name + ioqq + ioqqq + name + space + iuy)
+ii = int(input())
+print(ioq + name + ioqq + ioqqq + name + space + ioj)
+o = int(input())
+if o == 4 and ii == 0:
+    print("Syntax Error 9001 (Divide by 0)")
+elif o == 1:
+    a = int(i + ii)
+elif o == 2:
+    a = int(i - ii)
+elif o == 3:
+    a = int(i * ii)
+elif o == 4:
+    a = int(i / ii)
+else:
+    print("Syntax Error: 1000 (wrong Opperation)")
+    a = ("There was a error in the problem input")
+    
+print("The Answer is: ")
+print("______________\n")
+print(a)
+print("Thanks for using The [Baconsoft] Calculator")
+quit
